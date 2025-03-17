ebb30e943115e2ffc10a81942583b1dcd3eb357f
Reda Lemeden <git@redalemeden.com>
2025-03-17 21:11:15 +0100

    Add tailwind

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,9 @@
 /// <reference path="./.astro/types.d.ts" />
+/// <reference types="astro/client" />
 
 declare namespace App {
   interface Locals {
-    user: import('better-auth').User | null
-    session: import('better-auth').Session | null
+    user: import("better-auth").User | null;
+    session: import("better-auth").Session | null;
   }
 }
