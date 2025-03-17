803cdf5f5e0ddbcf32fc5ea0febbaa12dfec1ddd
Reda Lemeden <git@redalemeden.com>
2025-03-17 21:25:14 +0100

    Add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,19 @@ import {defineConfig} from "astro/config";
 import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   adapter: node({
     mode: "standalone",
   }),
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
