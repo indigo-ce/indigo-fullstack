efa506e316a557fcd495a77a2fc648da4e84011f
Reda Lemeden <git@redalemeden.com>
2025-03-30 16:20:43 +0200

    Fix email render bug

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import {betterAuth} from "better-auth";
 import {drizzleAdapter} from "better-auth/adapters/drizzle";
 import {db} from "@/db";
-import {render} from "astro:content";
+import {render} from "@react-email/render";
 import PasswordReset from "@/components/email/PasswordReset";
 import {sendEmail} from "./email";
 
